505d5561ced96295e96446a24f4c6cfd942feb18
Diego Pablo Matías Baltar <dpmbaltar@gmail.com>
2024-06-13 00:49:17 -0300

    Agregar implementación del árbol de decisión

--- tpo2/decision_tree.rb
+++ tpo2/decision_tree.rb
@@ -1,37 +1,149 @@
-require "tree"
+require "ruby-graphviz"
 
-# ..... Create the root node first.
-# ..... Note that every node has a name and an optional content payload.
-root_node = Tree::TreeNode.new("ROOT", "Root Content")
-root_node.print_tree
+module ML
 
-# ..... Now insert the child nodes.
-#       Note that you can "chain" the child insertions to any depth.
-root_node << Tree::TreeNode.new("CHILD1", "Child1 Content") << Tree::TreeNode.new("GRANDCHILD1", "GrandChild1 Content")
-root_node << Tree::TreeNode.new("CHILD2", "Child2 Content")
+  # Frecuencia de valores del atributo.
+  def self.attr_frequency(data, attr_name)
+    data.group_by { |row| row[attr_name] }.map { |k, v| [k, v.length] }.to_h
+  end
 
-# ..... Lets print the representation to stdout.
-# ..... This is primarily used for debugging purposes.
-root_node.print_tree
+  # Subconjunto de datos con con valor de atributo.
+  def self.data_subset(data, attr_name, attr_value)
+    data.select { |row| row[attr_name] == attr_value }
+  end
 
-# ..... Lets directly access children and grandchildren of the root.
-# ..... The can be "chained" for a given path to any depth.
-child1       = root_node["CHILD1"]
-grand_child1 = root_node["CHILD1"]["GRANDCHILD1"]
+  # Función de entropía.
+  def self.data_entropy(data, class_attr_name)
+    attr_freq = attr_frequency(data, class_attr_name)
+    attr_freq.transform_values do |freq|
+      prob = freq / data.length.to_f
+      prob * Math::log2(prob)
+    end.values.sum * -1
+  end
 
-# ..... Now retrieve siblings of the current node as an array.
-siblings_of_child1 = child1.siblings
+  # Entropía.
+  def self.attr_entropy(data, attr_name, class_attr_name)
+    sum = 0
+    attr_freq = attr_frequency(data, attr_name)
+    attr_freq.each do |aval, afreq|
+      subset = data_subset(data, attr_name, aval)
+      entropy_subset = data_entropy(subset, class_attr_name)
+      sum += (afreq / data.length.to_f) * entropy_subset
+    end
+    sum
+  end
 
-# ..... Retrieve immediate children of the root node as an array.
-children_of_root = root_node.children
+  # Ganancia de información.
+  def self.gain(data_entropy, attr_entropy)
+    data_entropy - attr_entropy
+  end
 
-# ..... Retrieve the parent of a node.
-parent = child1.parent
+  # Algoritmo Árbol de Decisión.
+  def self.decision_tree(data, attributes, tree_node, class_attr_name)
+    class_frequency = attr_frequency(data, class_attr_name)
+    if class_frequency.length == 1
+      tree_node.content = data[0][class_attr_name]
+    elsif attributes.length == 0
+      most_frequent_class = class_frequency.max_by { |key, value| value }[0]
+      tree_node.content = most_frequent_class
+    else
+      p0 = data_entropy(data, class_attr_name)
+      max_gain = { "gain" => 0, "attribute" => nil }
+      attributes.each do |attribute|
+        pi = attr_entropy(data, attribute, class_attr_name)
+        gain = p0 - pi
+        if gain > max_gain["gain"]
+          max_gain["gain"] = gain
+          max_gain["attribute"] =  attribute
+        end
+      end
 
-# ..... This is a depth-first and L-to-R pre-ordered traversal.
-root_node.each { |node| node.content.reverse }
+      if max_gain["gain"] < 0.05
+        most_frequent_class = class_frequency.max_by { |key, value| value }[0]
+        tree_node.content =  most_frequent_class
+      else
+        tree_node.content = max_gain["attribute"]
+        data_freq_by_attr = attr_frequency(data, max_gain["attribute"])
+        data_freq_by_attr.each do |attr_value, freq|
+          child = TreeNode.new(nil)
+          tree_node.add_child(attr_value, child)
+          data_with_value = data.select { |row| row[max_gain["attribute"]] == attr_value }
+          attributes_except = attributes.select { |row| row["name"] != max_gain["attribute"] }
+          decision_tree(data_with_value, attributes_except, child, class_attr_name)
+        end
+      end
+    end
+  end
 
-# ..... Remove a child node from the root node.
-root_node.remove!(child1)
+  # Nodo para contruir un árbol.
+  class TreeNode
 
-# .... Many more methods are available. Check out the documentation!
+    # Getters/setters
+    attr_accessor :content, :children
+
+    def initialize(content = nil)
+      @content = content
+      @children = []
+    end
+
+    def add_child(edge, child)
+      @children.push({ node: child, edge: edge })
+    end
+
+    def to_s
+      "(#{@content})"
+    end
+
+    def showChildren
+      @children.each do |child|
+        puts "\-> child with branch #{child[:edge]} is #{child[:node].to_s}"
+      end
+    end
+
+    def show_descendants
+      puts generate_tree_string(self, "", 0)
+    end
+
+    def save_graphviz(filename)
+      graph = GraphViz.new(:G, type: :digraph)
+      graph.node[:shape] = "box"
+      generate_graphviz(graph, self, nil)
+      graph.output(png: filename)
+    end
+
+    private
+
+    def generate_graphviz(graph = nil, tree_node = nil, parent = nil)
+      return if graph.nil? || tree_node.nil?
+
+      current = graph.add_nodes(tree_node.content)
+      graph.add_edges(parent, current) unless parent.nil?
+      tree_node.children.each do |child|
+        generate_graphviz(graph, child[:node], current)
+      end
+    end
+
+    def generate_tree_string(node, edge, level)
+      string = ""
+      if !node.nil?
+        if level != 0
+          string += " " * (level*3)
+          string += "|\n"
+          string += " " * (level*3)
+          string += "| [#{edge.to_s}]\n"
+          string += " " * (level*3)
+          string += "|__"
+          string += "#{node.to_s}\n"
+        elsif
+          string += "#{node.to_s}\n"
+        end
+
+        level += 1
+        node.children.each do |child|
+          string += generate_tree_string(child[:node], child[:edge], level)
+        end
+      end
+      string
+    end
+  end
+end
--- tpo2/main.rb
+++ tpo2/main.rb
@@ -1,8 +1,17 @@
 #!/usr/bin/env ruby
 
 require "csv"
+require "ruby-graphviz"
+require_relative "decision_tree"
 
-DATASET_FILE = "data/zoo.data"
+# Dataset
+DATASET_FILE = "credit.data"
 
-dataset = CSV.read(DATASET_FILE)
+dataset = CSV.parse(File.read(DATASET_FILE), headers: true)
+dataset_attrs = dataset.headers
+dataset_attrs.delete("class")
 
+tree = ML::TreeNode.new
+ML::decision_tree(dataset, dataset_attrs, tree, "class")
+tree.show_descendants
+tree.save_graphviz("tree.png")
